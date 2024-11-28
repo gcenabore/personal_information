723b283227a5ea205fee4141e11a53b03c2b2583
gcenabore <gianenabore30@gmail.com>
2024-11-28 12:08:13 +0800

    added additional questions for the user information

--- personal_information.py
+++ personal_information.py
@@ -10,4 +10,12 @@ def collect_information():
         phone_number = input("Please enter your Phone number: ")
         email = input("Please enter your email: ")
 
+        nationality = input("Enter your nationality: ")
+        complexion = input("Enter your complexion: ")
+        blood_type = input("Enter your blood type: ")
+        weight = input("Enter your weight in Kg: ")
+        height = input("Enter your height in cm: ")
+
+
+
 collect_information()
