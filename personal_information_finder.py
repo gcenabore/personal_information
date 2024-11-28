980cb5e4d92a5cd8b8093357b37d07a7678a4fac
gcenabore <gianenabore30@gmail.com>
2024-11-29 04:13:25 +0800

    find the user info using the user name

--- personal_information_finder.py
+++ personal_information_finder.py
@@ -3,8 +3,20 @@
 user_name = input("Enter Name to Find information: ")
 
 with open("personal_information.txt", "r") as f:
-    user_information = f.read()
-    print(user_information)
+    for line in f:
+        if user_name in line:
+            found = True
+            print("information found")
+            print(line)
+
+            for line in range(10):
+                print(next(f))
+            break
+
+
+    # user_information = f.read()
+    # print(user_information)
+
 
 
 
