8be29856d1c9d73f35dbbfd125e7ef329fc682cb
gcenabore <gianenabore30@gmail.com>
2024-11-28 12:03:52 +0800

    ask the user for the personal information

---
+++ personal_information.py
@@ -0,0 +1,13 @@
+
+
+def collect_information():
+
+    while True:
+        first_name = input("Please enter your First Name: ")
+        surname = input("Please enter your Surname: ")
+        age = input("Please enter your Age: ")
+        date_of_birth = input("Please enter your Birth date: ")
+        phone_number = input("Please enter your Phone number: ")
+        email = input("Please enter your email: ")
+
+collect_information()
