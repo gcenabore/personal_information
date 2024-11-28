034bda9856f34b549e4512d21036dd2a1dd6f8a3
gcenabore <gianenabore30@gmail.com>
2024-11-28 16:38:37 +0800

    combine first name to surname to have a full name and update valid criteria for phone number, age. and add valid criteria for birthdate

--- personal_information.py
+++ personal_information.py
@@ -1,34 +1,49 @@
-
+from datetime import datetime
 
 def collect_information():
 
-
     while True:
-        first_name = input("Please enter your First Name: ")
-        surname = input("Please enter your Surname: ")
-
-        while True:
-            age = input("Please enter your Age: ")
-            if age.isdigit():
-                digit = int(age)
-            else:
-                print("Invalid: enter a digit age")
-                break
         
-            date_of_birth = input("Please enter your Birth date: ")
+        while True:
+
+            first_name = input("Please enter your First Name: ")
+            surname = input("Please enter your Surname: ")
+
+            full_name = first_name + surname
 
             while True:
-                phone_number = input("Please enter your Phone number: ")
+                age = input("Please enter your Age: ")
 
-                if len(phone_number) != 11:
-                    print("Invalid: type a valid phone number (11 digits).")
+                if age.isdigit():
+                    digit = int(age)
                     break
-        email = input("Please enter your email: ")
+                else:
+                    print("Invalid: enter a digit age")
+                    
+            while True: 
+                date_of_birth = input("Please enter your Birth date: ")
+                try:
+                    datetime.strptime(date_of_birth, "%m/%d/%Y")
+                    break
+                except ValueError:
+                    print("Invalid: Pleaes enter your Birth date in this format (MM/DD/YYYY).")
+                        
+            while True:
+                phone_number = input("Please enter your Phone number: ")
+                number = int(phone_number)
 
-        nationality = input("Enter your nationality: ")
-        complexion = input("Enter your complexion: ")
-        blood_type = input("Enter your blood type: ")
-        weight = input("Enter your weight in Kg: ")
-        height = input("Enter your height in cm: ")
+                if len(phone_number) == 11:
+                    break
+                else:
+                    print("Invalid: type a valid phone number (11 digits).")
+
+            
+            # email = input("Please enter your email: ")
+
+            # nationality = input("Enter your nationality: ")
+            # complexion = input("Enter your complexion: ")
+            # blood_type = input("Enter your blood type: ")
+            # weight = input("Enter your weight in Kg: ")
+            # height = input("Enter your height in cm: ")
 
 collect_information()
