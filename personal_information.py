ddb141d1353185f315e8877c816c44b2857a6be6
gcenabore <gianenabore30@gmail.com>
2024-11-29 23:19:43 +0800

    updated the code, added try except for error handling

--- personal_information.py
+++ personal_information.py
@@ -9,90 +9,92 @@ def collect_information():
 
     blood_types = ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"]
 
-    with open("personal_information.txt", "a") as file:        
-
-        while True:
-
-            first_name = input("Please enter your First Name: ")
-            surname = input("Please enter your Surname: ")
-
-            full_name = first_name + " " + surname
+    try: 
+        with open("personal_information.txt", "a") as file:        
 
             while True:
-                age = input("Please enter your Age: ")
 
-                if age.isdigit():
-                    digit = int(age)
-                    break
-                else:
-                    print("Invalid: enter a digit age")
-                        
-            while True: 
-                date_of_birth = input("Please enter your Birth date: ")
-                try:
-                    datetime.strptime(date_of_birth, "%m/%d/%Y")
-                    break
-                except ValueError:
-                    print("Invalid: Pleaes enter your Birth date in this format (MM/DD/YYYY).")
-                                        
-            while True:
-                phone_number = input("Please enter your Phone number: ")
+                first_name = input("Please enter your First Name: ")
+                surname = input("Please enter your Surname: ")
 
-                if phone_number.isdigit() and len(phone_number) == 11:
-                    number = int(phone_number)
-                    break
-                else:
-                    print("Invalid: type a valid phone number (11 digits).")
-            
-            while True:
-                address = input("Please enter your address: ")
-                if address.strip():
-                    break
-                else:
-                    print("Ivalid: Address cannot be empty. Try again.")
+                full_name = first_name + " " + surname
 
-            email = input("Please enter your email: ")
+                while True:
+                    age = input("Please enter your Age: ")
 
-            while not validate_email(email):
-                print("Invalid format. Please try again.")
-                email = input("Enter email: ")
+                    if age.isdigit():
+                        digit = int(age)
+                        break
+                    else:
+                        print("Invalid: enter a digit age")
+                            
+                while True: 
+                    date_of_birth = input("Please enter your Birth date: ")
+                    try:
+                        datetime.strptime(date_of_birth, "%m/%d/%Y")
+                        break
+                    except ValueError:
+                        print("Invalid: Pleaes enter your Birth date in this format (MM/DD/YYYY).")
+                                            
+                while True:
+                    phone_number = input("Please enter your Phone number: ")
+
+                    if phone_number.isdigit() and len(phone_number) == 11:
+                        number = int(phone_number)
+                        break
+                    else:
+                        print("Invalid: type a valid phone number (11 digits).")
                 
-            nationality = input("Enter your nationality: ")
+                while True:
+                    address = input("Please enter your address: ")
+                    if address.strip():
+                        break
+                    else:
+                        print("Ivalid: Address cannot be empty. Try again.")
 
-            while True:
-                complexion = input("Enter your complexion (Fair skin/Medium skin/Light brown skin/Brown skin/Black skin): ")
-                if complexion in valid_skin_tone:
+                email = input("Please enter your email: ")
+
+                while not validate_email(email):
+                    print("Invalid format. Please try again.")
+                    email = input("Enter email: ")
+                    
+                nationality = input("Enter your nationality: ")
+
+                while True:
+                    complexion = input("Enter your complexion (Fair skin/Medium skin/Light brown skin/Brown skin/Black skin): ")
+                    if complexion in valid_skin_tone:
+                        break
+                    else:
+                        print("Invalid: Try again.")
+                while True:
+                    blood_type = input("Enter your blood type: ")
+                    if blood_type in blood_types:
+                        break
+                    else:
+                        print("Invalid: Try again. choose a valid blood type (A+/A-/B+/B-/AB+/AB-/O+/O-)")
+                            
+                weight = input("Enter your weight in Kg: ")
+
+                height = input("Enter your height in cm: ")
+
+                file.write(f"Full name: {full_name}\n")
+                file.write(f"Age: {age}\n")
+                file.write(f"Birthday: {date_of_birth}\n")
+                file.write(f"Phone number: {phone_number}\n")
+                file.write(f"Address: {address}\n")
+                file.write(f"email: {email}\n")
+                file.write(f"Nationality: {nationality}\n")
+                file.write(f"Complexion: {complexion}\n")
+                file.write(f"Blood type: {blood_type}\n")
+                file.write(f"Weight: {weight}\n")
+                file.write(f"Height: {height}\n")
+                file.write("-" * 100 + "\n")
+
+                another_entry = input("Do you want to enter another person? (Yes/No): ").strip().lower()
+                if another_entry != "yes":
                     break
-                else:
-                    print("Invalid: Try again.")
-            while True:
-                blood_type = input("Enter your blood type: ")
-                if blood_type in blood_types:
-                    break
-                else:
-                    print("Invalid: Try again. choose a valid blood type (A+/A-/B+/B-/AB+/AB-/O+/O-)")
-                        
-            weight = input("Enter your weight in Kg: ")
-
-            height = input("Enter your height in cm: ")
-
-            file.write(f"Full name: {full_name}\n")
-            file.write(f"Age: {age}\n")
-            file.write(f"Birthday: {date_of_birth}\n")
-            file.write(f"Phone number: {phone_number}\n")
-            file.write(f"Address: {address}\n")
-            file.write(f"email: {email}\n")
-            file.write(f"Nationality: {nationality}\n")
-            file.write(f"Complexion: {complexion}\n")
-            file.write(f"Blood type: {blood_type}\n")
-            file.write(f"Weight: {weight}\n")
-            file.write(f"Height: {height}\n")
-            file.write("-" * 100 + "\n")
-
-            another_entry = input("Do you want to enter another person? (Yes/No): ").strip().lower()
-            if another_entry != "yes":
-                break
-
+    except:
+        print(f"an error occured")                
             
 
 collect_information() 
