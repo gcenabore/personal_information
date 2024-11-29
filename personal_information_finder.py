007c292daaae4088b6ecc794f15dc4bff62fa0e2
gcenabore <gianenabore30@gmail.com>
2024-11-29 23:09:07 +0800

    update the print statements if found and not found. still figuring out some errors.

--- personal_information_finder.py
+++ personal_information_finder.py
@@ -9,15 +9,16 @@ with open("personal_information.txt", "r") as f:
     for line in f:
         if user_name in line:
             found = True
-            print("information found")
+            print(f"INFORMATION FOUND: Information on {user_name}:")
             print(line.strip())
 
-            for _ in range(4):
+            for line in range(10):
                 print(next(f).strip())
+            print("_" * 100)
             break
 
 if not found:
-    print("no information found.")
+    print(f"ERROR: THERE IS NO  EXISTING INFORMATION ON: {user_name}")
 
     # user_information = f.read()
     # print(user_information)
