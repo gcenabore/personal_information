6cc182f0d844b5d5587efbcd8b5d57ad853673c8
gcenabore <gianenabore30@gmail.com>
2024-11-29 23:32:09 +0800

    wrapped in while loop, continuosly asks the user to find information, otherwise exit.

--- personal_information_finder.py
+++ personal_information_finder.py
@@ -1,28 +1,33 @@
 
-# !!!code needs more revision, and update.!!!
 
-print("!!!TYPE THE USER'S FIRST NAME AND SURNAME TO ACQUIRE THEIR INFORMATION!!!")
+while True:
 
-first_name = input("Enter First name: ")
-surname = input("Enter Surname: ")
+    print("!!!TYPE THE USER'S FIRST NAME AND SURNAME TO ACQUIRE THEIR INFORMATION!!!")
 
-user_name = first_name + " " + surname
-found = False
+    first_name = input("Enter First name: ")
+    surname = input("Enter Surname: ")
 
-with open("personal_information.txt", "r") as f:
-    for line in f:
-        if user_name in line:
-            found = True
-            print(f"INFORMATION FOUND: Information on {user_name}:")
-            print(line.strip())
+    user_name = first_name + " " + surname
+    found = False
 
-            for line in range(10):
-                print(next(f).strip())
-            print("_" * 100)
-            break
+    with open("personal_information.txt", "r") as f:
+        for line in f:
+            if user_name in line:
+                found = True
+                print(f"INFORMATION FOUND: Information on {user_name}:")
+                print(line.strip())
 
-if not found:
-    print(f"ERROR: THERE IS NO  EXISTING INFORMATION ON: {user_name}")
+                for line in range(10):
+                    print(next(f).strip())
+                print("_" * 100)
+                break
+
+    if not found:
+        print(f"ERROR: THERE IS NO  EXISTING INFORMATION ON: {user_name}")
+    
+    retry = input("Do you want to search again? (Yes/No)").strip().lower()
+    if retry != "yes":
+        break
 
     # user_information = f.read()
     # print(user_information)
